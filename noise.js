64a9b3b980bb92f8695e2f8afbd10d87e4373d77
Drew Anderson <dtanders@horus.dreamhost.com>
2015-02-09 13:49:21 -0800

    Refactor

--- noise.js
+++ noise.js
@@ -1,52 +1,72 @@
 (function(AudioContext) {
-	AudioContext.prototype.createWhiteNoise = function(bufferSize) {
-		bufferSize = bufferSize || 4096;
-		var node = this.createScriptProcessor(bufferSize, 1, 1);
+	var MAX_BUFFER = 16384;
+	
+	function whiteNoiseData (bufferSize) {
+		return function () {
+			var data = [];
+			for (var i = 0; i < bufferSize; ++i) {
+				data[i] = Math.random() * 2 - 1;
+			}
+			return data;
+		}
+	}
+	
+	function redNoiseData (bufferSize) {
+		var b0, b1, b2, b3, b4, b5, b6, noise = whiteNoiseData(bufferSize);
+		b0 = b1 = b2 = b3 = b4 = b5 = b6 = 0.0;
+		return function () {
+			var acc, data = noise();
+			for (var i = 0; i < bufferSize; ++i) {
+				acc = data[i];
+				b0 = 0.99886 * b0 + acc * 0.0555179;
+				b1 = 0.99332 * b1 + acc * 0.0750759;
+				b2 = 0.96900 * b2 + acc * 0.1538520;
+				b3 = 0.86650 * b3 + acc * 0.3104856;
+				b4 = 0.55000 * b4 + acc * 0.5329522;
+				b5 = -0.7616 * b5 - acc * 0.0168980;
+				data[i] = (b0 + b1 + b2 + b3 + b4 + b5 + b6 + acc * 0.5362) * 0.11;
+				b6 = acc * 0.115926;
+			}
+			return data;
+		}
+	}
+	
+	function brownNoiseData (bufferSize) {
+		var last = 0.0, noise = whiteNoiseData(bufferSize);
+		return function () {
+			var acc, data = noise();
+			for (var i = 0; i < bufferSize; ++i) {
+				last = (last + (0.02 * data[i])) / 1.02;
+				data[i] = last * 3.5;
+			}
+			return data;
+		}
+	}
+	
+	function genericNode (ctx, dataFactory, bufferSizeIn) {
+		var bufferSize = bufferSizeIn || MAX_BUFFER,
+			 node = ctx.createScriptProcessor(bufferSize, 1, 1),
+			 dataGen = dataFactory(bufferSize);
+		
 		node.onaudioprocess = function(e) {
-			var output = e.outputBuffer.getChannelData(0);
-			for (var i = 0; i < bufferSize; i++) {
-				output[i] = Math.random() * 2 - 1;
+			var output = e.outputBuffer.getChannelData(0),
+				 data = dataGen();
+			for (var i = 0; i < bufferSize; ++i) {
+				output[i] = data[i];
 			}
 		}
 		return node;
+	}
+	
+	AudioContext.prototype.createWhiteNoise = function(bufferSize) {
+		return genericNode(this, whiteNoiseData, bufferSize);
 	};
 
 	AudioContext.prototype.createPinkNoise = function(bufferSize) {
-		bufferSize = bufferSize || 4096;
-		var b0, b1, b2, b3, b4, b5, b6;
-		b0 = b1 = b2 = b3 = b4 = b5 = b6 = 0.0;
-		var node = this.createScriptProcessor(bufferSize, 1, 1);
-		node.onaudioprocess = function(e) {
-			var output = e.outputBuffer.getChannelData(0);
-			for (var i = 0; i < bufferSize; i++) {
-				var white = Math.random() * 2 - 1;
-				b0 = 0.99886 * b0 + white * 0.0555179;
-				b1 = 0.99332 * b1 + white * 0.0750759;
-				b2 = 0.96900 * b2 + white * 0.1538520;
-				b3 = 0.86650 * b3 + white * 0.3104856;
-				b4 = 0.55000 * b4 + white * 0.5329522;
-				b5 = -0.7616 * b5 - white * 0.0168980;
-				output[i] = b0 + b1 + b2 + b3 + b4 + b5 + b6 + white * 0.5362;
-				output[i] *= 0.11; // (roughly) compensate for gain
-				b6 = white * 0.115926;
-			}
-		}
-		return node;
+		return genericNode(this, redNoiseData, bufferSize);
 	};
 
 	AudioContext.prototype.createBrownNoise = function(bufferSize) {
-		bufferSize = bufferSize || 4096;
-		var lastOut = 0.0;
-		var node = this.createScriptProcessor(bufferSize, 1, 1);
-		node.onaudioprocess = function(e) {
-			var output = e.outputBuffer.getChannelData(0);
-			for (var i = 0; i < bufferSize; i++) {
-				var white = Math.random() * 2 - 1;
-				output[i] = (lastOut + (0.02 * white)) / 1.02;
-				lastOut = output[i];
-				output[i] *= 3.5; // (roughly) compensate for gain
-			}
-		}
-		return node;
+		return genericNode(this, brownNoiseData, bufferSize);
 	};
 })(window.AudioContext || window.webkitAudioContext);
